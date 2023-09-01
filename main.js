e217b631f26a100d76eee72809e9bd1d01a3d4bc
Pulin Borua <137756919+pulinborua@users.noreply.github.com>
2023-09-01 23:04:02 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.continar1').hide();
+
+$('.show').click(function (){
+$('.continar1').show();
+});
+
+
+$('.hide').click(function (){
+$('.continar1').hide();
+})
+
+$('.remove').click(function (){
+$('.continar1').remove();
+})
